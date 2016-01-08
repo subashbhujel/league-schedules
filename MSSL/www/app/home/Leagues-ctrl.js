e3067e162144c68ae4557716c56f5ccdbba6ecdf
Subash Bhujel <subashbhujel@live.com>
2016-01-07 16:48:10 -0800

    Adding hardcoded data

---
+++ MSSL/www/app/home/Leagues-ctrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'user strict';
+
+    angular.module('starter').controller('LeaguesCtrl', ['leagueApi', LeaguesCtrl]);
+
+    function LeaguesCtrl(leagueApi) {
+        var vm = this;
+
+        var leagues = leagueApi.getLeagues();
+        var leagueData = leagueApi.getLeagueData();
+
+        console.log(leagues, leagueData);
+    };
+})();
